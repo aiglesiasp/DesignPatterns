18b9e549b54d4aad5915740bb5945552ee8b75f3
aiglesiasp <107586474+aiglesiasp@users.noreply.github.com>
2022-11-24 11:41:06 +0100

    Add files via upload

---
+++ DragonBallApp/DragonBallApp/UI/Home/HomeCellModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCellModel.swift
+//  DragonBallApp
+//
+//  Created by Aitor Iglesias Pubill on 18/11/22.
+//
+
+import Foundation
+
+struct HomeCellModel {
+    var image: String?
+    var name: String?
+    var isVillain: Bool
+    var description: String?
+    
+}
+
